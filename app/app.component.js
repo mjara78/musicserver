f13e350732ecd6b9e00c6687c6a7c6a9b80f9f8b
Manuel Jara <manuel.jara.oliver@gmail.com>
2017-04-09 12:46:57 +0000

    Added Material-Angular and basic structure for view

--- app/app.component.js
+++
@@ -1,9 +0,0 @@
-// /app/app.component.js
-
-export const AppComponent = {  
-  template: `
-    <ms-header></ms-header>
-    <div ui-view></div>
-    <ms-footer></ms-footer>
-  `
-}
--- app/components/components.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular'
-
-import genre from './genre'
-
-const components = angular  
-  .module('app.components', [genre])
-  .name
-
-export default components  
