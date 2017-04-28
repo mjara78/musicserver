2cbf6f4c422e7918517b98c337b1fa7ea933afec
Manuel Jara <manuel.jara.oliver@gmail.com>
2017-04-28 20:14:01 +0200

    Add service for app messages

--- server/public/src/common/common.module.js
+++ server/public/src/common/common.module.js
@@ -4,12 +4,30 @@ import angular from 'angular'
 import { HeaderComponent } from './header/header.component'  
 import { MenuComponent } from './menu/menu.component'
 import MenuService from './menu/menu.service'
+import MessageService from './message/message.service'
 
 const common = angular  
   .module('app.common', [])
   .service('MenuService', MenuService)
+  .service('MessageService', MessageService)
   .component('msHeader', HeaderComponent)
   .component('msMenu', MenuComponent)
+  .run(['Restangular', 'MessageService', 
+    function(Restangular, MessageService){
+      Restangular.setErrorInterceptor(
+          function(response) {
+
+            if (response.status == 418) { // Bussness Logic Error
+              MessageService.showMessage(response.data.message);
+            }
+            else { // Other Errors
+              MessageService.showMessageError(response.data);
+            }
+            
+            return false; // stop the promise chain
+          }
+      );
+    }])
   .name
 
 export default common
---
+++ server/public/src/common/message/message.service.js
@@ -0,0 +1,29 @@
+
+class MessageService {  
+  constructor ($mdToast) {
+    this.toast = $mdToast
+  }
+
+  showMessage (text, delay) {
+    if (!delay) {
+    	delay = 3000;
+    }
+    this.toast.show(
+      this.toast.simple()
+        .textContent(text)
+        .hideDelay(delay)
+      );
+  };
+   
+  showMessageError(error) {
+    this.toast.show(
+      this.toast.simple()
+        .textContent('Error: '+error.message)
+        .hideDelay(6000)
+      );
+  }
+}
+
+MessageService.$inject = ['$mdToast']
+
+export default MessageService
