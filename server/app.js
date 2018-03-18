b53d0e942485ef42c0558f36595356fa182df22c
Manuel Jara <manuel.jara.oliver@gmail.com>
2018-03-18 23:16:38 +0100

    Delimit max versions for some libraries. Remove jade dependence.

--- server/app.js
+++ server/app.js
@@ -28,8 +28,8 @@ if (process.env.NODE_ENV == "development"){
 app.use(compression());
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
