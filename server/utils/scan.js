c783be40370153aa4c06f855b9823e998c7fdf7a
Manuel Jara <manuel.jara.oliver@gmail.com>
2018-10-07 23:15:56 +0200

    Fix promises error that produced interruption of the process of refresh. Change tags library and read more tags.

--- server/utils/scan.js
+++ server/utils/scan.js
@@ -3,6 +3,7 @@ var fs = Promise.promisifyAll(require("fs"));
 var fsSync = require('fs');
 var path = require('path');
 var mm = require('musicmetadata');
+var mmeta = require('music-metadata');
 var songExists = require('../controllers/library.controller').songExists;
 
 var fileTypes = ['.mp3'];
@@ -10,7 +11,7 @@ var fileTypes = ['.mp3'];
 function extractMetadata(baseDir, callback) {
      return new Promise (function (resolve, reject) {
        
-        fs.readdirAsync(baseDir)
+    return fs.readdirAsync(baseDir)
           .map(processElement,{concurrency: 1 })
 					.reduce(function (total, elem) {
 							if ( elem > total){
@@ -20,8 +21,7 @@ function extractMetadata(baseDir, callback) {
 							}
 					})
 					.then(resolve)
-          .catch(reject);
-       
+          .catch(reject);      
        });
   
   function processElement (element) {
@@ -29,21 +29,21 @@ function extractMetadata(baseDir, callback) {
           
           var filePath = path.join(baseDir, element);
           var ext = path.extname(filePath);
-
-          fs.statAsync(filePath)
+  //   console.log("+file=>"+filePath)
+          return fs.statAsync(filePath)
             .then( function (stats) {
                  if (stats.isFile()) {
                      if (admittedType(ext)) {
-                        songExists(filePath)
+                        return songExists(filePath)
                           .then(function (exists) {
                             if (exists) {
-                        //      console.log("===========>>>>>>>> Filepath already exists:"+filePath)
+                          //    console.log("===========>>>>>>>> Filepath already exists:"+filePath)
                               return resolve(0); // If song already exists dont process it
                             } else { // filepath not exists at any song
                               // Read metadata from file
-                              readMetadata(filePath)
+                              return readMetadata(filePath)
                                 .then(function (tags) {
-                          //          console.log("===========>>>>>>>> Processing file:"+filePath)
+                                //    console.log("=====>>>> Processing file:"+filePath)
                                     return callback (filePath, tags).then(resolve);
                                 })
                                 .catch(reject);
@@ -53,6 +53,7 @@ function extractMetadata(baseDir, callback) {
                        return resolve(0);
                      }
                  } else { // is a directory
+           //         console.log("===========>>>>>>>> Scan new folder:" + filePath);
                     return extractMetadata(filePath, callback).then(resolve);
                  }
               })
@@ -71,91 +72,7 @@ function extractMetadata(baseDir, callback) {
         return false;
      }   
   }
-  
-  function readMetadata (filePath) {
-    return new Promise (function (resolve, reject) {
-      
-      var tagGenre;
-      var currentFile = filePath;
-      
-      var readableStream = fsSync.createReadStream(filePath);
-      var parser = mm(readableStream, { duration: true }, function (err, metadata) {
-        
-        if (err) {
-          console.log('Error reading metadata from ' + currentFile + '. ' + err);
-          
-          var tags = {
-            genre: 'unknow genre',
-            year: '0000',
-            artist: 'unknow artist',
-            album: 'unknow album',
-            albumArtist: 'unknow artist',
-            title: 'error reading',
-            track: '0',
-            duration: '0'
-          };
-        
-        } 
-        else {
-          // Genre
-          if (metadata.genre.length > 0) {
-            tagGenre = metadata.genre[0];	
-          }
-          else {
-            tagGenre = 'unknow genre';
-          }
-        
-          // Year
-          var year = metadata.year;
-        
-          // Artist
-          var artist;
-          if (metadata.artist.length > 0) {
-            artist = metadata.artist[0];	
-          }
-          else {
-            artist = 'unknow artist';
-          }
-        
-          // Album Artist
-          var albumArtist;
-          if (metadata.albumartist.length > 0) {
-            albumArtist = metadata.albumartist[0];	
-          }
-          else {
-            albumArtist = artist;
-          }
-        
-          // Album
-          var album = metadata.album; 
-        
-          // Title
-          var title = metadata.title;
-        
-          // Track
-          var track = metadata.track.no;
-        
-          // Duration
-          var duration = metadata.duration;
-        
-          readableStream.close();
-        
-          var tags = {
-            genre: tagGenre,
-            year: year,
-            artist: artist,
-            album: album,
-            albumArtist: albumArtist,
-            title: title,
-            track: track,
-            duration: duration
-          };
-        }    
-        
-        resolve(tags);
-      });
-    });
-  }
+
 }
 
 exports.extractMetadata = extractMetadata;
